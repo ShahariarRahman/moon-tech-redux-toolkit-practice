e35220c9afba63a9f9b75c49f3409a5e3202bd77
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-14 09:01:11 +0600

    Create a Redux State Slice: filterSlice

---
+++ src/features/filter/filterSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    stock: false,
+    brands: [],
+    keyword: "",
+};
+
+const filterSlice = createSlice({
+    name: "filter",
+    initialState,
+    reducers: {},
+});
